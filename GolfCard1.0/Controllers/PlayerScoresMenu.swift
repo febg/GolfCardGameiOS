c17d8e8140d3683d9c5cd1fb3a76288342e516f0
Felipe <felipeballesteros1@gmail.com>
2018-06-07 02:19:58 -0700

    Score menu refactor

---
+++ GolfCard1.0/Controllers/PlayerScoresMenu.swift
@@ -0,0 +1,21 @@
+//
+//  PlayerScoresMenu.swift
+//  GolfCard1.0
+//
+//  Created by Felipe Ballesteros on 2018-06-06.
+//  Copyright © 2018 Felipe Ballesteros. All rights reserved.
+//
+
+import UIKit
+
+class PlayerScoresMenu: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
